f012f745913009f295a1774fdf27637fbc8ea4d6
Darren Burgess <darrentburgess@gmail.com>
2016-08-27 11:23:46 -0400

    add database

--- news.rb
+++ news.rb
@@ -11,6 +11,22 @@ configure do
   set :session_secret, "asdf;lkjasdfk"
 end
 
+class News
+  include DataMapper::Resource
+
+  property :id, Serial, :required => false
+  property :url, String, :required => false
+  property :title, String, :required => false
+  property :date, Date, :required => false
+end
+
+def initialize_database
+  News.auto_upgrade!
+end
+
+initialize_database
+
 get "/" do
+  @news = News.all(:order => [:date.asc])
   erb :index
 end
